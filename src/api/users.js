70e10eddfe70884ef638d0cd2062ace160ff336d
DianaaaaAAA <17854239607@163.com>
2021-06-15 18:32:10 +0800

    update system-manage

---
+++ src/api/users.js
@@ -0,0 +1,24 @@
+import request from '@/utils/request'
+
+export function fetchUserList() {
+  return request({
+    url: '/tisp/user',
+    method: 'get'
+  })
+}
+
+export function createArticle(data) {
+  return request({
+    url: '/vue-element-admin/article/create',
+    method: 'post',
+    data
+  })
+}
+
+export function updateArticle(data) {
+  return request({
+    url: '/vue-element-admin/article/update',
+    method: 'post',
+    data
+  })
+}
